1dba26f6aa26e4220dbfa15f8f1f4427e48bbfa3
HenryV4 <igor.zaplatynsky@gmail.com>
2024-02-29 17:15:54 +0200

    Add_ASSIGNMENT_file_code_file_and_test_file

---
+++ lab1/peak.py
@@ -0,0 +1,34 @@
+
+def find_the_longest_peak(arr):
+    el_pos = 1
+    peak_start = None
+    peak_end = None
+    longest_peak_sequence = []
+    longest_peak_length = 0
+    while el_pos < len(arr) - 1:
+        if arr[el_pos - 1] < arr[el_pos] > arr[el_pos + 1]:
+            peak_start = el_pos - 1
+            peak_end = el_pos + 1
+               
+            left = peak_start - 1
+            while left >= 0 and arr[left] < arr[peak_start]:
+                peak_start = left
+                left -= 1
+                
+            right = peak_end + 1
+            while right < len(arr) and arr[right] < arr[peak_end]:
+                peak_end = right
+                right += 1
+                
+            peak_sequence = arr[peak_start:peak_end + 1]
+
+            if len(peak_sequence) > longest_peak_length:
+                longest_peak_length = len(peak_sequence)
+                longest_peak_sequence = peak_sequence
+
+        el_pos +=1
+
+    return longest_peak_sequence
+        
+arr = [4, 5, 3, 5, 6, 7, 4, 3, 10, 1, 6, 8, 10, 3, 3, 5, 7, 4, 3, 8, 3]
+print(find_the_longest_peak(arr))   
---
+++ lab1/test_peak.py
@@ -0,0 +1,23 @@
+import unittest
+import peak
+
+class TestPeak(unittest.TestCase):
+
+    def test_array_is_sorted_increasinly(self):
+        self.assertEqual(peak.find_the_longest_peak([1, 2, 3, 4, 5, 6, 7, 8]), [])
+    
+    def test_array_is_sorted_decreasinly(self):
+        self.assertEqual(peak.find_the_longest_peak([8, 7, 6, 5, 4, 3, 2, 1]), [])
+
+    def test_array_of_two_el(self):
+        self.assertEqual(peak.find_the_longest_peak([3,5]), [])
+    
+    def test_array_with_no_peaks(self):
+        self.assertEqual(peak.find_the_longest_peak([1, 1, 1, 1, 1, 1, 1, 1]), [])
+    
+    def test_array_with_three_peaks(self):
+        self.assertEqual(peak.find_the_longest_peak([1, 2, 1, 2, 1, 2, 1]), [1, 2, 1])
+
+
+if  __name__ == '__main__':
+    unittest.main()
