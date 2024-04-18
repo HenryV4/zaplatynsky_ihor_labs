8cec693c7ff0e51b19e6f0b15368c466f4177bf1
HenryV4 <igor.zaplatynsky@gmail.com>
2024-04-18 17:19:05 +0300

    lab6

---
+++ src/penguins_city_gas.py
@@ -0,0 +1,30 @@
+
+
+def dfs(graph, start, visited):
+    visited.add(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+def check_gas_delivery(cities_list, storages_list, pipe_list):
+    graph = {city: [] for city in cities_list + storages_list}
+    
+    for pipe in pipe_list:
+        source, destination = pipe
+        graph[source].append(destination)
+
+    visited = set()
+    dfs(graph, storages_list[0], visited)
+
+    unreachable_cities = [city for city in cities_list if city not in visited]
+
+    return unreachable_cities
+
+
+
+
+cities = ["Lviv", "Kyiv", "Rivne", "Dnipro", "Odesa"]
+storages = ["Storage_1", "Storage_2"]
+pipelines = [["Storage_1", "Lviv"], ["Lviv", "Rivne"], ["Lviv", "Kyiv"], ["Lviv", "Odesa"]]
+print(check_gas_delivery(cities, storages, pipelines))
+
---
+++ test/test_penguins_city_gas.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+sys.path.append('C:/WORK/PROG/2/lab/src')
+from penguins_city_gas import check_gas_delivery
+
+class TestAggressiveCows(unittest.TestCase):
+    def test_1(self):
+        cities = ["Lviv", "Kyiv", "Rivne", "Dnipro", "Odesa"]
+        storages = ["Storage_1", "Storage_2"]
+        pipelines = [["Storage_1", "Lviv"], ["Lviv", "Rivne"], ["Lviv", "Kyiv"], ["Lviv", "Odesa"]]
+        self.assertEqual(check_gas_delivery(cities, storages, pipelines), ["Dnipro"])
+    def test_no_cities(self):
+        cities = []
+        storages = ["Storage_1", "Storage_2"]
+        pipelines = [["Storage_1", "Storage_2"]]
+        self.assertEqual(check_gas_delivery(cities, storages, pipelines), [])
+    def test_2(self):
+        cities = ["Lviv", "Kyiv", "Rivne", "Dnipro", "Odesa"]
+        storages = ["Storage_1", "Storage_2"]
+        pipelines = [["Storage_1", "Storage_2"], ["Lviv", "Rivne"], ["Lviv", "Kyiv"], ["Lviv", "Odesa"]]
+        self.assertEqual(check_gas_delivery(cities, storages, pipelines), ["Lviv", "Kyiv", "Rivne", "Dnipro", "Odesa"])
+
+if __name__ == '__main__':
+    unittest.main()
+
