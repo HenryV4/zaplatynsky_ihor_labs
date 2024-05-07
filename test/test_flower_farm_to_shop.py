698081b6defa38e41763d8c796d8758111390a9f
HenryV4 <150185495+HenryV4@users.noreply.github.com>
2024-05-07 13:08:02 +0300

    Lab8 (#9)
    * Add_ASSIGMENT_for_lab8_code_and_test
    
    * Pylint_modifications_in_code_file

---
+++ test/test_flower_farm_to_shop.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+
+sys.path.append("C:/projects/zaplatynsky_ihor_labs/src")
+from flower_farm_to_shop import calculate_max_flow
+
+
+class TestMaxFlow(unittest.TestCase):
+    def test_max_flow_1(self):
+        expected_max_flow = 51
+        actual_max_flow = calculate_max_flow(
+            "C:/projects/zaplatynsky_ihor_labs/resources/roads.csv"
+        )
+        self.assertEqual(actual_max_flow, expected_max_flow)
+
+    def test_max_flow_2(self):
+        expected_max_flow = 0
+        actual_max_flow = calculate_max_flow(
+            "C:/projects/zaplatynsky_ihor_labs/resources/roads_no_way.csv"
+        )
+        self.assertEqual(actual_max_flow, expected_max_flow)
+
+
+if __name__ == "__main__":
+    unittest.main()
