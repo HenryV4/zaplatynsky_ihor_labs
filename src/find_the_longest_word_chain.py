10358462e4862e14af592aaf90d64486707bf849
HenryV4 <igor.zaplatynsky@gmail.com>
2024-05-16 19:32:48 +0300

    Add_ASSIGMENT_for_lab9_code_test_and_resources

---
+++ src/find_the_longest_word_chain.py
@@ -0,0 +1,55 @@
+def longest_word_chain(words):
+    """Find the length of the longest word chain in a list of words.
+
+    Args:
+        words (list): A list of strings representing words.
+
+    Returns:
+        max_chain_lengths (int): The length of the longest word chain.
+    """
+    words.sort(key=lambda word: -len(word))
+
+    max_chain_lengths = [1] * len(words)
+
+    for i in range(len(words)):
+        current_word = words[i]
+        for j in range(len(current_word)):
+            new_word = current_word[:j] + current_word[j + 1 :]
+            if new_word in words:
+                index_of_new_word = words.index(new_word)
+                max_chain_lengths[index_of_new_word] = max(
+                    max_chain_lengths[index_of_new_word], max_chain_lengths[i] + 1
+                )
+
+    return max(max_chain_lengths)
+
+
+def read_file(file_path):
+    """Reads words from a file.
+
+    Args:
+        file_path (str): The path to the input file.
+
+    Returns:
+        words (list): A list of strings representing words.
+    """
+    words = []
+    with open(file_path, "r") as file:
+        num_words = int(file.readline().strip())
+        for _ in range(num_words):
+            word = file.readline().strip()
+            words.append(word)
+
+    return words
+
+
+def write_output(max_chain_lengths, file_path):
+    """Writes the maximum chain lengths to a file.
+
+    Args:
+        max_chain_lengths (int): The maximum chain lengths.
+        file_path (str): The path to the output file.
+
+    """
+    with open(file_path, "w") as file:
+        file.write(str(max_chain_lengths))
